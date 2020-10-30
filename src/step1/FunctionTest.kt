111fd9099f6504d37f8c685f55274e321322230c
skykec4 <>
2020-10-30 18:03:42 +0900

    funcTest

---
+++ src/step1/FunctionTest.kt
@@ -0,0 +1,15 @@
+package step1
+
+fun main(args: Array<String>) {
+
+        //6분 30초부터
+}
+
+
+fun funByReturn(s: String): String {
+    return s
+}
+
+fun funByParam(i: Int, s: String) {
+    println("$i + $s")
+}
